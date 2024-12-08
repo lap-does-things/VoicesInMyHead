4ed3eed9a382d55b6d54b8f901280d5e1d1aef3f
lap-does-things <laptokgaming7@gmail.com>
2024-12-09 02:28:54 +0300

    VOICESINMYHEAD

--- voices.py
+++ voices.py
@@ -3,7 +3,7 @@ from time import sleep
 from random import choice
 a,n,v = open("voice.voice", mode='r').read().splitlines()[:912][0:],open("voice.voice", mode='r').read().splitlines()[:7694][912:], open("voice.voice", mode='r').read().splitlines()[:8736][7694:]
 system('cls' if name == 'nt' else 'clear')
-while True:
+while True: 
     voice = "The " +choice(a) + " " + choice(n)+ " " + choice(v)+ "s the " + choice(n)+". "
     for i in range(len(voice)):
         print(voice[i], end='', flush=True)
